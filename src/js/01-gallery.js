511e2f8c0fca342d301f4603162c7790cfc29304
Sergey Donyk <doncarlos@i.ua>
2023-02-20 00:28:45 +0200

    Update-1-task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 
 const container = document.querySelector('.gallery')
-
+container.addEventListener('click', onPictureClick)
 
     const markup = galleryItems.map(({preview, original, description}) => 
         
@@ -21,12 +21,34 @@ const container = document.querySelector('.gallery')
     ).join('')
   container.insertAdjacentHTML('beforeend', markup)
 
+  function onPictureClick(Event) {
+    Event.preventDefault();
+    const isPictureItam = Event.target.classList.contains('gallery__image');
+    if (!isPictureItam ) return;
 
-function onPictureCreateModal = (Event) => {
-    if (Event.target.classlist.value !== 'gallery__image')
-        return
-}
-// Event.target.parentNode.Propagation()
-// basiclightbox.create(`<img src="${Event.target.dataset.soursce}>`).show();
-// container.addEventListener('click', onClickElement)
-// container.insertAdjacentHTML('beforeend', createGalleryMarcup())
+    const currentUrl = Event.target.dataset.source;
+
+    const instance = basicLightbox.create(
+      `
+		<img src="${currentUrl}" width="1280" height="auto"/>
+        `,
+      {
+        onShow: (instance) => {
+          window.addEventListener('keydown', onEscKeyPress);
+        },
+        onClose: (instance) => {
+          window.removeEventListener('keydown', onEscKeyPress);
+        },
+      }
+    );
+    instance.show();
+
+    function onEscKeyPress(e) {
+      const ESC_KEY_CODE = 'Escape';
+      const isEscKey = e.code === ESC_KEY_CODE;
+      if (!isEscKey) return;
+      instance.close();
+    }
+  }
+
+
