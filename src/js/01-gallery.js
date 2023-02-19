95db2a3f5e59420dc0974795e3a5cf5ca1ed9775
Sergey Donyk <doncarlos@i.ua>
2023-02-19 23:28:52 +0200

    second-task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const container = document.querySelector('.gallery')
+
+
+    const markup = galleryItems.map(({preview, original, description}) => 
+        
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src = "${preview}"
+      data-source="${original}"
+      alt="${description}"  
+    />
+  </a>
+</div>
+`
+    ).join('')
+  container.insertAdjacentHTML('beforeend', markup)
+
+
+function onPictureCreateModal = (Event) => {
+    if (Event.target.classlist.value !== 'gallery__image')
+        return
+}
+// Event.target.parentNode.Propagation()
+// basiclightbox.create(`<img src="${Event.target.dataset.soursce}>`).show();
+// container.addEventListener('click', onClickElement)
+// container.insertAdjacentHTML('beforeend', createGalleryMarcup())
